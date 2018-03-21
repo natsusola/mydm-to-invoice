49fff19fb08d220284cba31c8b8e727b0f5e801b
Natsu <natsu@jcd.tw>
2018-03-21 17:17:46 +0800

    增加 pro 模式

--- src/main/main.js
+++ src/main/main.js
@@ -7,7 +7,7 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 const windowStateKeeper = require('electron-window-state');
-require('electron-debug')({ showDevTools: true });
+require('electron-debug')({ showDevTools: process.env.NODE_ENV !== 'production' });
 
 let mainWindow;
 let win;
@@ -18,17 +18,17 @@ if (process.env.NODE_ENV !== 'development') {
 
 const winURL = process.env.NODE_ENV !== 'production'
   ? 'http://localhost:9090'
-  : `file://${path.resolve(__dirname, 'renderer')}/index.html`;
+  : `file://${path.resolve(__dirname)}/index.html`;
 
 function createWindow() {
   let mainWindowState = windowStateKeeper({
-    defaultWidth: 900,
-    defaultHeight: 900
+    defaultWidth: 600,
+    defaultHeight: 600
   });
 
   mainWindow = new BrowserWindow({
-    width: 900,
-    height: 900,
+    width: 600,
+    height: 600,
     x: mainWindowState.x,
     y: mainWindowState.y
   });
